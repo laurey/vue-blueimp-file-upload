a518025e17dc111930b3be9171183af04176f82f
laureyap <laureyap@yahoo.com>
2017-04-10 22:08:54 +0800

    Initial commit

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container is-info is-outlined">
+    <form id="fileupload">
+      <a class="button is-primary is-outlined">
+        Add Files
+        <input class="input is-transparent" type="file" placeholder="File" name="files[]" />
+      </a>
+    </form>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import 'blueimp-file-upload'
+import VueBlu from 'vue-blu'
+import 'vue-blu/dist/css/vue-blu.min.css'
+Vue.use(VueBlu)
+
+export default {
+  name: 'fileupload',
+  data () {
+    return {
+        msg: ''
+    }
+  }
+}
+</script>
